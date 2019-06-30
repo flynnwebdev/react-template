788b9c68e66b83001c45bec4fd91d95d42d80d63
Flynn <flynnwebdev@gmail.com>
2019-06-30 12:26:50 +1000

    React minimal template initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function App(props) {
+    return (
+        <h1>Hello {props.name}!</h1>
+    )
+}
+
+ReactDOM.render(<App name="Matt" />, document.querySelector('#app'))
